bd008c08e106c96a5475afaf005990a5cbfdc5b1
Bhargavi <Bvyas5788@conestogac.on.ca>
2019-01-30 19:24:08 -0500

    final commit

---
+++ QA-Assignment-1-Test/QA1_Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1;
+using NUnit.Framework;
+
+namespace QA_Assignment_1_Test
+{
+    [TestFixture]
+     class QA1_Test
+    {
+        [Test]
+        public void SetWidth_input3_expectWidthEquals3()
+        {
+            //Arrange
+            int l = 4;
+            int w = 3;
+//int ln=newln;
+            Rectangle testRectangle = new Rectangle();
+
+            //Act
+            int newwidth = testRectangle.SetWidth(w);
+
+            //Assert
+            Assert.AreEqual(newwidth, 3);
+
+        }
+
+    }
+}
